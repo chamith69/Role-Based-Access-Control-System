67242994a614a0a0d532ee0a4640c58bd0caebae
Chamith <chamithdakshina21613@gmail.com>
2025-02-11 00:56:39 +0530

    Add Permission Checking Function

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -13,14 +13,17 @@ interface AuthState {
   setUser: (user: User | null) => void;
   setRoles: (roles: string[]) => void;
   setPermissions: (permissions: string[]) => void;
+  hasPermission: (permission: string) => boolean;
 }
 
-export const useAuthStore = create<AuthState>((set) => ({
+export const useAuthStore = create<AuthState>((set, get) => ({
   user: null,
   roles: [],
   permissions: [],
   setUser: (user) => set({ user }),
   setRoles: (roles) => set({ roles }),
   setPermissions: (permissions) => set({ permissions }),
+  hasPermission: (permission) => get().permissions.includes(permission),
 }));
+
 
