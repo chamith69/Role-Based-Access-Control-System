6ef3fb0e48299bdc6d9ddaa0f783e9d467d6ec1b
Chamith <chamithdakshina21613@gmail.com>
2025-02-11 00:52:37 +0530

    Add Roles and Permissions

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -3,14 +3,24 @@ import { create } from 'zustand';
 interface User {
   id: string;
   email: string;
+  roles: string[];
 }
 
 interface AuthState {
   user: User | null;
+  roles: string[];
+  permissions: string[];
   setUser: (user: User | null) => void;
+  setRoles: (roles: string[]) => void;
+  setPermissions: (permissions: string[]) => void;
 }
 
 export const useAuthStore = create<AuthState>((set) => ({
   user: null,
+  roles: [],
+  permissions: [],
   setUser: (user) => set({ user }),
+  setRoles: (roles) => set({ roles }),
+  setPermissions: (permissions) => set({ permissions }),
 }));
+
