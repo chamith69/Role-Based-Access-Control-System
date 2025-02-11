9804d2ac30fa577b51bce82272d15b824236b80e
Chamith <chamithdakshina21613@gmail.com>
2025-02-11 12:33:29 +0530

    Add Layout Component for Protected Routes

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,16 +2,22 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
+import Users from './pages/Users';
+import Layout from './components/Layout';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="users" element={<Users />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
+
