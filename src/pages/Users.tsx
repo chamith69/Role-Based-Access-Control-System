77297ed1b3379c9d6aa0f952d500a5d3180883fa
Chamith <chamithdakshina21613@gmail.com>
2025-02-13 07:15:59 +0530

    Fix and Edit Errors

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React , { useState, useEffect } from 'react';
 import { supabase } from '../lib/supabase';
 import { UserPlus, Pencil, Trash2, Shield } from 'lucide-react';
 
